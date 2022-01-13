103fdf20008984345cb8a8f750662356ff9a4f80
kai <it_01boy@163.com>
2022-01-13 23:18:46 +0800

    git-test second commit!

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Git Test!");
+        System.out.println("Git Test2!");
+        System.out.println("Git Test3!");
     }
 }
