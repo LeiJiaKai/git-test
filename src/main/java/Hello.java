cf1886a9544593577e1417bb987bb0d0e5bcb3ec
kai <it_01boy@163.com>
2022-01-13 23:28:25 +0800

    hot-fix commit2!

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
         System.out.println("Git Test2!");
         System.out.println("Git Test3!");
         System.out.println("Git Test4!");
+        System.out.println("hot-fix!");
     }
 }
